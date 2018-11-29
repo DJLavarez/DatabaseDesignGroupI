a59bc5059e77023bb9039f1777d354fbb091fad1
Darwin John Lavarez <42482562+DJLavarez@users.noreply.github.com>
2018-11-29 11:39:48 +0800

    Update DatabaseGroupD.sql
    Added foreign Keys

--- DatabaseGroupD.sql
+++ DatabaseGroupD.sql
@@ -1,61 +1,137 @@
-create database DatabaseGroupD;
-use DatabaseGroupD;
+create database DatabaseGroupI;
+use DatabaseGroupI;
 
 create table register_city_name(
 	id int auto_increment primary key,
-    city_name varchar(255)
-;
-
-create table register_town_name(
-	id int auto_increment primary key,
-    town_name varchar(255)
-;
+    city_name varchar(255),
+    city_population int,
+    num_family_c int
+);
 
 create table register_barangay(
 	id int auto_increment primary key,
-    barangay_name varchar(255)
-;
-
-create table register_district_zone(
-	id int auto_increment primary key,
-    district_name varchar(255),
-    zone_num int
-;
-
-create table register_population(
-	id int auto_increment primary key,
-    population_num int,
-    population_children int,
-    population_pwd int,
-    population_seniorctzn int
-;
-
-create table register_vehicle_population(
-	id int auto_increment primary key,
-    vehicle_population int
-;
+    barangay_name varchar(255),
+    barangay_population int,
+    num_family_b int,
+    city_id INT, 
+    FOREIGN KEY (city_id),
+    	REFERENCES register_barangay(id)
+);
 
 create table register_family_name(
 	id int auto_increment primary key,
     family_name varchar(255),
-    person_info varchar(255),
-    average_age int,
-    sex_gender bool,
-    edu_lvl varchar(255),
+    family_size int,
+    num_children int,
+    barangay_id INT,
+    FOREIGN KEY (barangay_id)
+	REFERENCES register_family_name(id)
+);
+
+create table register_person(
+	id int auto_increment primary key,
+    person_name varchar(255),
+    person_age int,
+    person_gender bool,
     marital_status varchar(255),
     person_occupation varchar(255),
-    person_religion varchar(255),
-	family_size int,
-    num_children int,
+    family_id INT,
+    FOREIGN KEY (family_id)
+	REFERENCES register_person(id)
+);
+
+INSERT INTO register_city_name(city_name, city_population, num_family_c)
+VALUES
+    ('Caloocan', '', ''),
+    ('Pasay', '', ''),
+    ('Quezon', '', ''),
+    ('Malabon', '', ''),
+    ('Valenzuela', '', ''),
+    ('Las Piñas', '', ''),
+    ('Pasig', '', ''),
+    ('Navotas', '', ''),
+    ('Taguig', '', ''),
+    ('Manila', '', ''),
+    ('Makati', '', ''),
+    ('Marikina', '', ''),
+    ('Parañaque', '', ''),
+    ('Mandaluyong', '', ''),
+    ('Cebu', '', ''),
+    ('Zamboanga', '', ''),
+    ('Baguio', '', ''),
+    ('Iloilo', '', ''),
+    ('Davao', '', ''),
+    ('Dagupan', '', '')
 ;
 
-create table register_average_incomes(
-	id int auto_increment primary key,
-    average_income int
+INSERT INTO register_barangay(barangay_name, barangay_population, num_family_b)
+VALUES
+	('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', '')
 ;
 
-create table register_house_address(
-	id int auto_increment primary key,
-    house_address varchar(255)
+INSERT INTO register_family_name(family_name, family_size, num_children)
+VALUES
+	('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', '')
 ;
-
+ 
+INSERT INTO register_person(person_name, person_age, person_gender, marital_status, person_occupation)
+VALUES
+	('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', ''),
+    ('', '', '')
+;
